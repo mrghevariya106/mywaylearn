d5ef01de5a665bb398a1d6394a3793bc2a14ebcd
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2022-03-16 20:20:31 +0530

    we have added databaseuser function

---
+++ lib/services/crud/notes_service.dart
@@ -0,0 +1,30 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+import 'package:path/path.dart' show join;
+
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person, ID=$id, Email=$email';
+
+  @override
+  bool operator ==(covariant DatabaseUser object) => id == object.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const idColumn = 'id';
+const emailColumn = 'email';
